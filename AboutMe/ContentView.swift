9faed71bd8dc2ad5e04903298dca2c1901e7b785
ginaiscoding <rpaek88@gmail.com>
2022-07-05 14:59:48 -0700

    Added about me profile section with images and title

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            
+            ZStack(alignment: .leading) {
+                Image("laptop")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Image("profile")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100)
+                .clipShape(Circle())
+                .padding(.bottom, -100)
+                .offset(y: 175)
+            }
+            .padding(.bottom)
+            VStack(alignment: .leading, spacing: 4) {
+                HStack {
+                    Text("Regina Paek")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                    Image(systemName: "checkmark.seal.fill")
+                        .foregroundColor(.blue)
+                        .imageScale(.large)
+                }
+                Text("@Gina_onTwitter")
+                    .font(.caption)
+                
+                Text("Bio: I don't have a real twitter")
+                    .fontWeight(.light)
+                    .padding(.top)
+            }
+            .padding(.horizontal)
+            Spacer()
+        }
     }
 }
 
